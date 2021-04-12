c581356b0b0279eca4d68e9e68a1354ca088d72f
Huldar <huldarh@gmail.com>
2021-04-12 09:58:03 +0000

    Merkja inn hluta

--- main.m
+++ main.m
@@ -1,1 +1,21 @@
 % adalskrain fyrir daemi 10 i sveiflufraedi
+
+% Hluti 1
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Skjámyndakerfi með hnappa fyrir mælingu,
+% stillingu á söfnunartíðni og fjölda mæligilda.
+% Sýnir niðurstöður FFT greiningar
+
+
+
+
+
+
+% Hluti 2
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Samskipti við A/D breytu
+
+
+% Hluti 3
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Greiningarhluti
